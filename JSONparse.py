c7cf0bb66d7e1c063a32361eba6044d77e3550d7
abhi-cool-karni <abhishekkul2000@gmail.com>
2023-08-12 19:00:26 +0530

    2 commit

--- JSONparse.py
+++ JSONparse.py
@@ -8,3 +8,23 @@ dict_course = json.loads(course)
 print(type(dict_course))
 print(dict_course)
 print(dict_course['name'])
+# get me the first language thought by trainer
+# list_language = dict_course['languages']
+# print(list_language)
+# print(type(list_language))
+# print(list_language[0])
+
+# shorthand
+print(dict_course['languages'][0])
+
+# ******** Parse content present in JSON file
+
+with open("C:\\Users\\Abhishek Kulkarni\\Desktop\\AK\\Software Testing\\course.json") as f:
+    data = json.load(f)
+    print(data)
+    # print(type(data))
+    # print(data['courses'])
+    print(data['courses'][1]['title'])
+    print(data['dashboard'])
+    print(type(data['dashboard']))
+    print(data['dashboard']['website'])
